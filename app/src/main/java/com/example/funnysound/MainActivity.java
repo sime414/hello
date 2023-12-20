744759c8b55aecf10ba272a189b0068df4c3c937
sime414 <nafiz2655@gmail.com>
2023-12-20 17:42:25 +0600

    new button

--- app/src/main/java/com/example/funnysound/MainActivity.java
+++ app/src/main/java/com/example/funnysound/MainActivity.java
@@ -1,88 +1,81 @@
 package com.example.funnysound;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.annotation.SuppressLint;
-import android.content.Context;
+import android.media.AudioAttributes;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
-import android.net.Uri;
-import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
-import android.view.LayoutInflater;
+import android.util.Log;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.BaseAdapter;
 import android.widget.Button;
-import android.widget.ListView;
-import android.widget.ProgressBar;
-import android.widget.TextView;
-import android.widget.Toast;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.funnysound.R;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
-    MediaPlayer mediaPlayer;
-    ProgressBar progressBar;
-    Button btnPlay;
-    String musicUrl = "https://firebasestorage.googleapis.com/v0/b/puzzlesonline-a2563.appspot.com/o/biodynamic-impact-braam-tonal-dark-176441.mp3?alt=media&token=277d3f43-fb92-4eeb-bbe3-0c624ea38d09";
 
+    private MediaPlayer mediaPlayer;
+    Button hello;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        hello = findViewById(R.id.hello);
 
-        btnPlay = findViewById(R.id.btnPlay);
-        progressBar = findViewById(R.id.progressBar);
+        @SuppressLint({"LocalSuppress", "MissingInflatedId"}) Button playButton = findViewById(R.id.playButton);
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                playAudioFromUrl("https://firebasestorage.googleapis.com/v0/b/puzzlesonline-a2563.appspot.com/o/biodynamic-impact-braam-tonal-dark-176441.mp3");
+            }
+        });
+        hello.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
-        mediaPlayer = new MediaPlayer();
-        mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
-
-        btnPlay.setOnClickListener(v -> {
-            new LoadMusicFromUrl().execute(musicUrl);
+            }
         });
 
-    } // onCreate method end here =============
 
-    private class LoadMusicFromUrl extends AsyncTask<String,Integer,Boolean> {
-
-        @Override
-        protected Boolean doInBackground(String... strings) {
-            try {
-                mediaPlayer.setDataSource(strings[0]);
-                mediaPlayer.prepare();;
-                return true;
-            } catch (Exception e){
-                e.printStackTrace();
-                return false;
-            }
-        }
-
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-            progressBar.setVisibility(View.VISIBLE);
-        }
-
-        @Override
-        protected void onPostExecute(Boolean success) {
-            super.onPostExecute(success);
-            progressBar.setVisibility(View.GONE);
-            if (success){
-                mediaPlayer.start();
-            } else {
-                Toast.makeText(MainActivity.this, "Something went wrong", Toast.LENGTH_SHORT).show();
-            }
-        }
-    } // LoadMusicFromUrl end here =============
-
-    @Override
-    protected void onDestroy() {
-        if (mediaPlayer != null){
-            mediaPlayer.stop();
-        }
-        super.onDestroy();
     }
-} // public class end here ====================
+
+    private void playAudioFromUrl(String url) {
+        if (mediaPlayer == null) {
+            mediaPlayer = new MediaPlayer();
+            mediaPlayer.setAudioAttributes(buildAudioAttributes());
+            mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+            try {
+                mediaPlayer.setDataSource(url);
+                mediaPlayer.prepare(); // might take long! (for buffering, etc)
+            } catch (IOException e) {
+                Log.e("MediaPlayer", "Error setting data source", e);
+            }
+
+            mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                @Override
+                public void onCompletion(MediaPlayer mediaPlayer) {
+                    // Release the MediaPlayer resources after playback is complete
+                    mediaPlayer.release();
+                    mediaPlayer = null;
+                }
+            });
+
+            mediaPlayer.start(); // no need to call prepare(); create() does that for you
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    private AudioAttributes buildAudioAttributes() {
+        return new AudioAttributes.Builder()
+                .setUsage(AudioAttributes.USAGE_MEDIA)
+                .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+                .build();
+    }
+}
